4b054e0b3a1ea7e3a5d1e1ec92d663217c0eb7a8
Niraj <niraj@Nirajs-MacBook-Pro.local>
2026-02-09 23:42:32 +0530

    Fix account login synchronization using visibility wait

--- src/test/java/AccountTests.java
+++ src/test/java/AccountTests.java
@@ -16,7 +16,9 @@ import java.util.Map;
 public class AccountTests extends BaseTest {
 
     @Test(retryAnalyzer = RetryAnalyzer.class)
-    public void TC_ACCOUNT_01_validateUpdateAccountDetails() throws IOException, ParseException, InterruptedException {
+    public void TC_ACCOUNT_01_validateUpdateAccountDetails()
+            throws IOException, ParseException {
+
         HomePage homePage = new HomePage(driver);
         LoginPage loginPage = new LoginPage(driver);
         AccountPage accountPage = new AccountPage(driver);
@@ -24,53 +26,49 @@ public class AccountTests extends BaseTest {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         JavascriptExecutor js = (JavascriptExecutor) driver;
 
+        // ---------- Launch & Login ----------
         homePage.launchHomePage();
-
-        wait.until(ExpectedConditions.visibilityOf(homePage.getUserIcon()));
-        js.executeScript("arguments[0].scrollIntoView(true);", homePage.getUserIcon());
         js.executeScript("arguments[0].click();", homePage.getUserIcon());
-
         WaitUtils.waitForElementToBeInvisible(driver);
 
         Map<String, String> userLoginDetails = JsonReader.getJsonMap("existingUser");
-        loginPage.inputLoginFormUserName.sendKeys(userLoginDetails.get("userName"));
-        loginPage.inputLoginFormPassword.sendKeys(userLoginDetails.get("password"));
-
-        wait.until(ExpectedConditions.visibilityOf(loginPage.loginFormSignInButton));
-        js.executeScript("arguments[0].scrollIntoView(true);", loginPage.loginFormSignInButton);
-        js.executeScript("arguments[0].click();", loginPage.loginFormSignInButton);
+        loginPage.login(
+                userLoginDetails.get("userName"),
+                userLoginDetails.get("password")
+        );
 
         wait.until(ExpectedConditions.visibilityOf(loginPage.loggedInUserName));
 
-        wait.until(ExpectedConditions.visibilityOf(homePage.getUserIcon()));
-        js.executeScript("arguments[0].scrollIntoView(true);", homePage.getUserIcon());
+        // ---------- Navigate to My Account ----------
         js.executeScript("arguments[0].click();", homePage.getUserIcon());
-
         wait.until(ExpectedConditions.visibilityOf(accountPage.myAccountButton));
-        js.executeScript("arguments[0].scrollIntoView(true);", accountPage.myAccountButton);
         js.executeScript("arguments[0].click();", accountPage.myAccountButton);
 
         wait.until(ExpectedConditions.visibilityOf(accountPage.myAccountContainer));
 
-        wait.until(ExpectedConditions.visibilityOf(accountPage.accountEditButton));
-        js.executeScript("arguments[0].scrollIntoView(true);", accountPage.accountEditButton);
+        // ---------- Edit Account ----------
         js.executeScript("arguments[0].click();", accountPage.accountEditButton);
-
         wait.until(ExpectedConditions.visibilityOf(accountPage.accountDetailsForm));
 
-        wait.until(ExpectedConditions.visibilityOf(accountPage.cityDetails)).clear();
+        accountPage.cityDetails.clear();
         accountPage.cityDetails.sendKeys(userLoginDetails.get("city"));
 
-        wait.until(ExpectedConditions.visibilityOf(accountPage.saveButton));
-        js.executeScript("arguments[0].scrollIntoView(true);", accountPage.saveButton);
         js.executeScript("arguments[0].click();", accountPage.saveButton);
 
+        // ---------- Validation ----------
         wait.until(ExpectedConditions.visibilityOf(accountPage.updateSuccessfulMessage));
-        softAssert.assertTrue(accountPage.updateSuccessfulMessage.isDisplayed(), "Account was not updated successfully");
+        softAssert.assertTrue(
+                accountPage.updateSuccessfulMessage.isDisplayed(),
+                "Account was not updated successfully"
+        );
 
         wait.until(ExpectedConditions.visibilityOf(accountPage.myAccountContainer));
-        softAssert.assertTrue(accountPage.myAccountContainer.isDisplayed(), "Failed to redirect on My Account page");
+        softAssert.assertTrue(
+                accountPage.myAccountContainer.isDisplayed(),
+                "Failed to redirect to My Account page"
+        );
 
         System.out.println("TC_ACCOUNT_01_validateUpdateAccountDetails passed successfully");
     }
+
 }
